8dd251aa6f24a04a10e901310b233b13e05fbd17
Andrey Lukin <aimm.legate@gmail.com>
2018-09-05 15:18:31 +0500

    fix errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import fs from 'fs';
-
 import { formatUrl, formatToFileName, getAllSrcFromHtml, filterLocalLinks, getFilename, transformAllSrcInHtml } from './utils';
 
 const { promises: fsPromises } = fs;
@@ -15,9 +14,9 @@ const saveAsFile = (url, path = '/') => axios
   .get(url)
   .then((resp) => {
     const fileName = formatToFileName(formatUrl(url), 'html');
-    const srcs = getAllSrcFromHtml(resp.data);
-    const filtredSrcs = filterLocalLinks(srcs);
-    filtredSrcs.forEach(link => saveFile(`${url}/${link}`, path));
+    const srcs = getAllSrcFromHtml(resp.data)
+      |> filterLocalLinks;
+    srcs.forEach(link => saveFile(`${url}/${link}`, path));
     fsPromises.writeFile(`${path}/${fileName}`, transformAllSrcInHtml(resp.data));
   })
   .catch(e => console.error(e));
